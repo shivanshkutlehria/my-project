11dcc35a02e03b66c59c342b175a7790885e5bb9
shivanshkutlehria <kutlehriashivansh2006@gmail.com>
2025-09-14 20:48:28 +0530

    Experiment-8

---
+++ React-project/Experiment-8/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import LibraryManagement from './component/LibraryManagement'
+
+function App() {
+  return (
+    <div className="App">
+      <LibraryManagement />
+    </div>
+  )
+}
+
+export default App
